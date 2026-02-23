1ca0d13c43006fd483ffcaaef32fbba5ab98c063
sital-tharu <sitatharu769@gmail.com>
2026-02-23 16:44:49 +0530

    import the DEPENDENCY

---
+++ app/rag.py
@@ -0,0 +1,6 @@
+from langchain_chroma import Chroma
+from langchain_ollama import OllamaEmbeddings, ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+
